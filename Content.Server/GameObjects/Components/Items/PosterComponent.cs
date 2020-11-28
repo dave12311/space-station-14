5e5f96d9780ad56b108d0998ceb608f0b36259a4
Dave12311 <horvath.david987@gmail.com>
2020-11-29 00:08:40 +0100

    Add poster textures, component and prototype

---
+++ Content.Server/GameObjects/Components/Items/PosterComponent.cs
@@ -0,0 +1,45 @@
+﻿using Content.Shared.Interfaces.GameObjects.Components;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Log;
+using Robust.Shared.Prototypes;
+using Robust.Shared.IoC;
+using Robust.Shared.Serialization;
+using Robust.Shared.Interfaces.Random;
+using Robust.Server.GameObjects;
+using System.Linq;
+using Robust.Shared.Random;
+
+namespace Content.Server.GameObjects.Components.Items
+{
+    [RegisterComponent]
+    public class PosterComponent : Component
+    {
+        [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+        [Dependency] private readonly IRobustRandom _random = default!;
+
+        public override string Name => "Poster";
+
+        private bool _contraband;
+
+        private PosterPrototype _prototype;
+
+        public override void ExposeData(ObjectSerializer serializer)
+        {
+            base.ExposeData(serializer);
+            serializer.DataField(ref _contraband, "contraband", false);
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            // Select random prototype and set entity name
+            System.Collections.Generic.List<PosterPrototype> prototypes;
+            prototypes = _prototypeManager.EnumeratePrototypes<PosterPrototype>()
+                .Where((_contraband ? p => p.Contraband : p => !p.Contraband))
+                .ToList();
+            _prototype = _random.Pick(prototypes);
+            Owner.Name += " - " + _prototype.Name;
+        }
+    }
+}
---
+++ Content.Server/GameObjects/Components/Items/PosterPrototype.cs
@@ -0,0 +1,34 @@
+﻿using Robust.Shared.Localization;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+using YamlDotNet.RepresentationModel;
+
+namespace Content.Server.GameObjects.Components.Items
+{
+    [Prototype("poster")]
+    public class PosterPrototype : IPrototype, IIndexedPrototype
+    {
+        public string ID { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Image { get; private set; }
+        public bool Contraband { get; private set; }
+
+        public void LoadFrom(YamlMappingNode mapping)
+        {
+            ID = mapping.GetNode("id").AsString();
+            Name = Loc.GetString(mapping.GetNode("name").AsString());
+            Image = mapping.GetNode("image").AsString();
+
+            if (mapping.TryGetNode("description", out var node))
+            {
+                Description = Loc.GetString(node.AsString());
+            }
+
+            if (mapping.TryGetNode("contraband", out node))
+            {
+                Contraband = node.AsBool();
+            }
+        }
+    }
+}
